1570673d6c93bc77fd6c695e684efdc142601df0
act2498 <117634708+act2498@users.noreply.github.com>
2023-01-24 21:59:56 +0800

    Create kick.lua

---
+++ kick.lua
@@ -0,0 +1,26 @@
+--hook to lib/managers/chatmanager
+--change 'kick_on'
+
+function ChatManager:receive_message_by_peer(channel_id, peer, message)
+	local color_id = peer:id()
+	local color = tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors]
+	local rank = peer:level() == nil and managers.experience:current_rank() or peer:rank()
+	local icon = managers.experience:rank_icon(rank)
+	local name = peer:name()
+
+	kick_on = {
+		'a',
+		'b',
+		'c'
+	}
+	for _, v in pairs(kick_on) do
+		if string.find(name,v) or string.find(message,v) then
+			managers.network:session():send_to_peers("kick_peer", peer:id(), message_id)
+			managers.network:session():on_peer_kicked(peer, peer:id(), message_id)
+			return
+		end
+	end
+
+	self:_receive_message(channel_id, name, message, tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors], icon, peer)
+end
+
