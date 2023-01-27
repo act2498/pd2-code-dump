1d98ea0472a40ca5b875342c0c0710e2dd725ae7
act2498 <117634708+act2498@users.noreply.github.com>
2023-01-28 04:55:28 +0800

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -24,3 +24,33 @@ function ChatManager:receive_message_by_peer(channel_id, peer, message)
 	self:_receive_message(channel_id, name, message, tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors], icon)
 end
 
+--version 2
+
+function ChatManager:receive_message_by_peer(channel_id, peer, message)
+	local color_id = peer:id()
+	local color = tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors]
+	local rank = peer:level() == nil and managers.experience:current_rank() or peer:rank()
+	local icon = managers.experience:rank_icon(rank)
+	local name = peer:name()
+
+	check = {
+		'a',
+		'b',
+		'c'
+	}
+	for _, v in pairs(check) do
+		if string.find(name,v) or string.find(message,v) then
+			if string.find(name,v) then
+				name=string.gsub(name,v,'*')
+			end
+			if string.find(message,v) then
+				message=string.gsub(message,v,'*')
+			end
+		end
+	end
+
+	self:_receive_message(channel_id, name, message, tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors], icon)
+end
+
+
+
