38bd7f4e889580c9ca5a054c59ffdd3657e67199
act2498 <117634708+act2498@users.noreply.github.com>
2023-02-02 09:13:49 +0800

    Update txt.lua

--- txt.lua
+++ txt.lua
@@ -1,5 +1,5 @@
 --run as keybind or plug in your function
-
+--https://payday-2-blt-docs.readthedocs.io/en/latest/lua/quick_menu/
 local menu_options = {}
 menu_options[1] = {text = "quit", is_cancel_button = true}
 local menu = QuickMenu:new("title", "message", menu_options)
